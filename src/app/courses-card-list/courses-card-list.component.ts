76840526f2a6ed2ef882396b1b86135d897fb35f
Ailye <42967809+Ailye@users.noreply.github.com>
2025-04-03 16:28:09 +0200

    suite et fin

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -1,37 +1,67 @@
-import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
-import {Course} from "../model/course";
+import {
+  Component,
+  inject,
+  Inject,
+  Input,
+  OnInit,
+  ViewEncapsulation,
+} from "@angular/core";
+import { Course } from "../model/course";
 import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
+import { openEditCourseDialog } from "../course-dialog/course-dialog.component";
+import { filter } from "rxjs/operators";
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
 
 @Component({
-    selector: 'courses-card-list',
-    templateUrl: './courses-card-list.component.html',
-    styleUrls: ['./courses-card-list.component.css'],
-    standalone: false
+  selector: "courses-card-list",
+  templateUrl: "./courses-card-list.component.html",
+  styleUrls: ["./courses-card-list.component.css"],
+  standalone: false,
 })
 export class CoursesCardListComponent implements OnInit {
+  @Input()
+  courses: Course[];
 
-    @Input()
-    courses: Course[];
+  cols = 3;
+  rowHeight = "500px";
+  handsetPortrait = false;
 
-    constructor() {
-    }
+  constructor() {}
 
-    ngOnInit() {
+  private dialog = inject(MatDialog);
+  private responsive = inject(BreakpointObserver);
 
-    }
+  ngOnInit() {
+    this.responsive
+      .observe([
+        Breakpoints.TabletPortrait,
+        Breakpoints.TabletLandscape,
+        Breakpoints.HandsetPortrait,
+        Breakpoints.HandsetLandscape,
+      ])
+      .subscribe((result) => {
+        this.cols = 3;
+        this.rowHeight = "500px";
+        this.handsetPortrait = false;
 
-    editCourse(course:Course) {
+        const breakpoints = result.breakpoints;
+        if (breakpoints[Breakpoints.TabletPortrait]) {
+          this.cols = 1;
+        } else if (breakpoints[Breakpoints.HandsetPortrait]) {
+          this.cols = 1;
+          this.rowHeight = "430px";
+          this.handsetPortrait = false;
+        } else if (breakpoints[Breakpoints.HandsetLandscape]) {
+          this.cols = 1;
+        } else if (breakpoints[Breakpoints.TabletLandscape]) {
+          this.cols = 2;
+        }
+      });
+  }
 
-
-    }
-
+  editCourse(course: Course) {
+    openEditCourseDialog(this.dialog, course)
+      .pipe(filter((val) => !!val))
+      .subscribe((val) => console.log("new course value", val));
+  }
 }
-
-
-
-
-
-
-
-
-
--- src/app/virtual-scrolling/virtual-scrolling.component.ts
+++ src/app/virtual-scrolling/virtual-scrolling.component.ts
@@ -1,19 +1,13 @@
-import {Component, OnInit} from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-    selector: "virtual-scrolling",
-    templateUrl: 'virtual-scrolling.component.html',
-    styleUrls: ["virtual-scrolling.component.scss"],
-    standalone: false
+  selector: "virtual-scrolling",
+  templateUrl: "virtual-scrolling.component.html",
+  styleUrls: ["virtual-scrolling.component.scss"],
+  standalone: false,
 })
 export class VirtualScrollingComponent implements OnInit {
+  items = Array.from({ length: 100000 }).map((value, i) => `Item #${i}`);
 
-  items = Array.from({length: 100}).map((value, i) => `Item #${i}`);
-
-  ngOnInit() {
-
-  }
-
-
+  ngOnInit() {}
 }
